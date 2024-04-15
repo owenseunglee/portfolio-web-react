b3fbd7e5665042e4514ecc80678ee40441507109
Owen <owenseunglee@gmail.com>
2024-04-14 19:38:55 -0700

    header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-
+/* import nav links data from constants */
+import { headerNav } from '../constants';
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header id="header"role="banner">
+            <div className="header_inner">
+                <div className="header_logo">
+                    <a href="/">Portfolio<em>react</em></a>
+                </div>
+                <nav className="header_nav"role="navigation"aria-label="Main">
+                    {/* iterate nav links */}
+                    <ul>
+                        {headerNav.map((nav,key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className="header_nav_mobile"
+                id="headerToggle"
+                aria-controls="primary-menu"
+                aria-expanded="false"
+                role="button"
+                tabindex="0"
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
     );
 };
 
---
+++ src/constants/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+export const headerNav = [
+    {
+        title: 'Intro',
+        url: '#intro'
+    },
+    {
+        title: 'Skill',
+        url: '#skill'
+    },
+    {
+        title: 'Site',
+        url: '#site'
+    },
+    {
+        title: 'Portfolio',
+        url: '#port'
+    },
+    {
+        title: 'Contact',
+        url: '#contact'
+    }
+];
+
+const Header = () => {
+    const[show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
+    return (
+        <header id="header" role="banner">
+            <div className="header_inner">
+                <div className="header_logo">
+                    <a href="/">Portfolio<em>react</em></a>
+                </div>
+                <nav className={`header_nav ${show ? "show" : ""}`} role="navigation" aria-label="Main">
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className="header_nav_mobile"
+                id="headerToggle"
+                aria-controls="primary-menu"
+                aria-expanded={show?"true":"false"}
+                role="button"
+                tabindex="0"
+                onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
